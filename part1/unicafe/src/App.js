9ff0482caaf7ae6c53ae2332022f5f7084f02200
Olmonator <olmo.lecht@posteo.de>
2021-12-30 05:02:21 +0200

    part 1.6 complete

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+const Stat = (props) => {
+  return (
+    <div>
+      <p>
+        {props.name}: {props.counter}
+      </p>
+    </div>
+  )
+}
+
+const Button = (props) => { 
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+  return (
+    <div>
+      <Header name={"give review"}/>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <Header name={"statistics"}/>
+      <Stat name={"Good"} counter={good}/>
+      <Stat name={"Neutral"} counter={neutral}/>
+      <Stat name={"Bad"} counter={bad}/>
+    </div>
+  )
+}
+
+export default App
