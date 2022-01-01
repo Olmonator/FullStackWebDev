f7427e0ea3312a94c688a9aec00bb95f17fc75cb
Olmonator <olmo.lecht@posteo.de>
2022-01-01 16:56:07 +0200

    part2.8 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,25 +3,29 @@ import Person from './components/Person'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
-      id: 1
+      id: 1,
+      number: '000 111'
     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     console.log(event.target)
     event.preventDefault()
     
     const nameObject = {
       name: newName,
+      number: newNumber,
       id: persons.length +1
     }
     if (persons.map(person => person.name).includes(newName)) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
-      console.log('add new person:', nameObject.name)
+      console.log('add new person:', nameObject.name, nameObject.number)
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -30,15 +34,26 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input 
             value={newName}
             onChange={handleNameChange}
           />
+        </div>
+        <div>
+          number: <input 
+            value={newNumber}
+            onChange={handleNumberChange}
+            />
         </div>
         <div>
           <button type="submit">add</button>
@@ -47,8 +62,9 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <li key={person.id}> {person.name} </li>
+          <li key={person.id}> {person.name}: {person.number}</li>
         )}
+
       </ul>
       <div>debug: {newName}</div>
     </div>
