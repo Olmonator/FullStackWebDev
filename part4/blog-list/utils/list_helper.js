f447c795a0b404718794e2d452aade7bea0fba33
Olmonator <olmo.lecht@posteo.de>
2022-01-04 06:02:31 +0200

    part4.5 complete: favorite tests sucess

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -15,6 +15,21 @@ const likes = (blogs) => {
     return blogs.map(blog => blog.likes).reduce((p, t) => p + t)
 }
 
+const favorite = (blogs) => {
+    console.log("TEST: blogs", blogs)
+    if (blogs.length === 0) {
+        console.log("TEST: empty bloglist")
+        return {}
+    }
+    if (blogs.length === 1) {
+        console.log("TEST: blocklist has only one entry")
+        return blogs[0]
+    }
+    const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+    console.log("TEST: maxLikes", maxLikes)
+    return blogs.find(blog => blog.likes === maxLikes)
+}
+
 module.exports = {
-  dummy, likes
+  dummy, likes, favorite
 }
