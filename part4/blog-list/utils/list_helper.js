40be2ae5d925201982a0c5bcb161fe103e5cc73e
Olmonator <olmo.lecht@posteo.de>
2022-01-04 05:34:52 +0200

    part4.3 completed: tests completed

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const likes = (blogs) => {
+    console.log("TEST: blogs", blogs)
+    if (blogs.length === 0) {
+        console.log("TEST: empty bloglist")
+        return 0
+    }
+    if (blogs.length === 1) {
+        console.log("TEST: blocklist has only one entry")
+        return blogs[0].likes
+    }
+    return blogs.map(blog => blog.likes).reduce((p, t) => p + t)
+}
+
 module.exports = {
-  dummy
+  dummy, likes
 }
