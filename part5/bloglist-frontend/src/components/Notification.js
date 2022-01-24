f27bcb07fca2876f8815bbfb057976de6823b71b
Olmonator <olmo.lecht@posteo.de>
2022-01-24 16:06:00 +0200

    part5.4 completed and ready to push

---
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -0,0 +1,20 @@
+import React from 'react' 
+import './../App.css'
+
+const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    } else {
+        if (message.substring(0,5) === 'Error') {
+            return <div className='error'>
+                {message}
+            </div>
+        } else {
+            return <div className='alert'>
+                {message}
+            </div>
+        }
+    }
+}
+
+export default Notification
