d7009f622435017b6c0156ffd11adaf3b90da452
Olmonator <olmo.lecht@posteo.de>
2022-01-31 22:12:20 +0200

    part7.7 completed [time: 60 minutes]

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if (name !== '') {
+      axios
+      .get(`https://restcountries.com/v2/name/${name}?fullText=true`)
+      .then(response => {
+        const found = true
+        if (response.data.status === 404) {
+          console.log('Country not Found')
+          setCountry({found: false})
+        } else {
+          setCountry({... response, found})
+          console.log('Country found:', {...response, found})
+        }
+      })
+    }
+  }, [name])
+  console.log(country.data)
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data[0].name} </h3>
+      <div>capital {country.data[0].capital} </div>
+      <div>population {country.data[0].population}</div> 
+      <img src={country.data[0].flag} height='100' alt={`flag of ${country.data[0].name}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
