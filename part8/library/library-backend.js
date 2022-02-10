d0033b2d1f062e711738e7a015c33ebb34d1af1e
Olmonator <olmo.lecht@posteo.de>
2022-02-10 16:12:23 +0200

    exercise 8.2 completed [time: 15 minutes]

--- part8/library/library-backend.js
+++ part8/library/library-backend.js
@@ -85,16 +85,25 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+      title: String!
+      author: String!
+      published: Int!
+      gernes: [String!]!
+  }
+
   type Query {
       bookCount: Int!
       authorCount: Int!
+      allBooks: [Book!] 
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
