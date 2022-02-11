da2b233999b3edbf4e75d1cf0e6108f95c30571e
Olmonator <olmo.lecht@posteo.de>
2022-02-11 14:17:37 +0200

    exercise 8.8 completed

---
+++ part8/library-frontend/src/App.js
@@ -0,0 +1,51 @@
+import { gql, useQuery } from '@apollo/client'
+import React, { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+const ALL_AUTHORS = gql`
+query {
+  allAuthors {
+    name
+    born
+    bookCount
+  }
+}
+`
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+
+  const result = useQuery(ALL_AUTHORS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  console.log(result)
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+        authors={result.data.allAuthors}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
+}
+
+export default App
